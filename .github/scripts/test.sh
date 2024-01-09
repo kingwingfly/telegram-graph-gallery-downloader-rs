f9c358de571c0bd311495a6adefb8e9b9df4de15
Louis <836250617@qq.com>
2024-01-09 19:42:27 +0800

    action

---
+++ .github/scripts/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cargo clippy --all-targets --all-features -- -D warnings
+
+cargo test
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,15 @@ mod parser;
 
 type BoxResult<T> = Result<T, Box<dyn std::error::Error>>;
 
+/// Download images from telegram graph gallery
+/// # Example
+/// ```no_run
+/// use telegram_graph_downloader::download;
+///
+/// # async fn run() {
+/// download("dir_to_save", "https://telegra.ph/xxx").await.unwrap();
+/// # }
+/// ```
 pub async fn download<P, S>(dir: P, url: S) -> BoxResult<()>
 where
     P: AsRef<str>,
